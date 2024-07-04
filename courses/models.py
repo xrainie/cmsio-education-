9de8f296303f4768395472c9b0ed73fe236bc31c
xoxrainie <xoxrainie@gmail.com>
2024-07-04 15:25:32 +0300

    create project & create courses app

---
+++ courses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    
+    class Meta:
+        ordering = ['title']
+    
+    def __str__(self):
+        return self.title
+    
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses_created')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
